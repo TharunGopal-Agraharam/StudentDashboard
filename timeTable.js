6c4f1232d61e325da729ef85b530dae090a8e899
TharunGopal-Agraharam <136571842+TharunGopal-Agraharam@users.noreply.github.com>
2023-08-08 22:09:09 +0530

    Add files via upload

---
+++ timeTable.js
@@ -0,0 +1,211 @@
+const Sunday =[
+    {   
+        time: 'Sunday',
+        roomNumber: 'Holiday',
+        subject: 'No class Available',
+        type: ''
+    }
+]
+const Monday =[
+    {   
+        time: '09:10-10:10 AM',
+        roomNumber: 'WB-217',
+        subject: 'SE',
+        type: 'Lecture'
+    },
+    {   
+        time: '10:10-11:10 AM',
+        roomNumber: 'WB-217',
+        subject: 'EB',
+        type: 'Lecture'
+    },
+    {   
+        time: '11:10-12:10 AM',
+        roomNumber: 'WB-217',
+        subject: 'DBMS',
+        type: 'Lecture'
+    },
+    {   
+        time: '01:00-02:00 PM',
+        roomNumber: 'WB-219',
+        subject: 'CN',
+        type: 'Lecture'
+    },
+    {   
+        time: '02:00-05:00 PM',
+        roomNumber: 'SEMINAR HALL-C',
+        subject: 'TECH TRAINING',
+        type: 'Training'
+    }
+]
+const Tuesday =[
+    {   
+        time: '09:10-10:10 AM',
+        roomNumber: 'WB-217',
+        subject: 'SDVL',
+        type: 'Lecture'
+    },
+    {   
+        time: '10:10-11:10 AM',
+        roomNumber: 'WB-217',
+        subject: 'SE',
+        type: 'Lecture'
+    },
+    {   
+        time: '11:10-12:10 AM',
+        roomNumber: 'WB-217',
+        subject: 'ML',
+        type: 'Lecture'
+    },
+    {   
+        time: '01:00-02:00 PM',
+        roomNumber: 'WB-219',
+        subject: 'CN',
+        type: 'Lecture'
+    },
+    {   
+        time: '02:00-05:00 PM',
+        roomNumber: 'CB LAB-05',
+        subject: 'DBMS LAB',
+        type: 'Practical'
+    }
+]
+
+const Wednesday =[
+    {   
+        time: '09:10-10:10 AM',
+        roomNumber: 'WB-217',
+        subject: 'ML',
+        type: 'Lecture'
+    },
+    {   
+        time: '10:10-11:10 AM',
+        roomNumber: 'WB-217',
+        subject: 'APTITUDE',
+        type: 'Lecture'
+    },
+    {   
+        time: '11:10-12:10 AM',
+        roomNumber: 'WB-217',
+        subject: 'DBMS',
+        type: 'Lecture'
+    },
+    {   
+        time: '01:00-04:00 PM',
+        roomNumber: 'WB-LAB-222',
+        subject: 'SOC(NP)',
+        type: 'Practical'
+    },
+    {   
+        time: '04:00-05:00 PM',
+        roomNumber: 'WB-LAB-222',
+        subject: 'DM',
+        type: 'Lecture'
+    }
+]
+
+const Thursday =[
+    {   
+        time: '09:10-12:10 AM',
+        roomNumber: 'CB LAB-05',
+        subject: 'ML LAB',
+        type: 'Practical'
+    },
+    {   
+        time: '01:00-02:00 PM',
+        roomNumber: 'WB-217',
+        subject: 'SDVL',
+        type: 'Lecture'
+    },
+    {   
+        time: '02:00-03:00 PM',
+        roomNumber: 'WB-217',
+        subject: 'INT',
+        type: 'Lecture'
+    },
+    {   
+        time: '03:00-04:00 PM',
+        roomNumber: 'WB-217',
+        subject: 'DBMS',
+        type: 'Lecture'
+    },
+    {   
+        time: '04:00-05:00 PM',
+        roomNumber: 'WB-217',
+        subject: 'ML',
+        type: 'Lecture'
+    }
+]
+
+const Friday =[
+    {   
+        time: '09:10-10:10 AM',
+        roomNumber: 'WB-218',
+        subject: 'CN',
+        type: 'Lecture'
+    },
+    {   
+        time: '10:10-11:10 AM',
+        roomNumber: 'WB-218',
+        subject: 'EB',
+        type: 'Lecture'
+    },
+    {   
+        time: '11:10-12:10 AM',
+        roomNumber: 'WB-218',
+        subject: 'SST',
+        type: 'Training'
+    },
+    {   
+        time: '01:00-02:00 PM',
+        roomNumber: 'WB-218',
+        subject: 'ML',
+        type: 'Lecture'
+    },
+    {   
+        time: '02:00-03:00 PM',
+        roomNumber: 'WB-218',
+        subject: 'SE',
+        type: 'Lecture'
+    },
+    {   
+        time: '03:00-04:00 PM',
+        roomNumber: 'WB-218',
+        subject: 'SDVL',
+        type: 'Lecture'
+    },
+    {   
+        time: '04:00-05:00 PM',
+        roomNumber: 'WB-218',
+        subject: 'EB',
+        type: 'Lecture'
+    }
+
+]
+
+const Saturday =[
+    {   
+        time: '09:10-10:10 AM',
+        roomNumber: 'WB-218',
+        subject: 'CN',
+        type: 'Lecture'
+    },
+    {   
+        time: '10:10-11:10 AM',
+        roomNumber: 'WB-218',
+        subject: 'DBMS',
+        type: 'Lecture'
+    },
+    {   
+        time: '11:10-12:10 AM',
+        roomNumber: 'WB-218',
+        subject: 'DM',
+        type: 'Lecture'
+    },
+    {   
+        time: '01:00-05:00 PM',
+        roomNumber: 'WB-219',
+        subject: 'Summer Internship-1',
+        type: 'Practical'
+    }
+]
